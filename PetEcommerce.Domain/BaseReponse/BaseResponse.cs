75ef1628ba157a78c51dedcfd18d6665de75fe48
Yusuf Mostafa <yusufmustafa1@outlook.com>
2025-11-28 15:11:20 +0200

    [Add] BaseResponse and ResponseHandler

---
+++ PetEcommerce.Domain/BaseReponse/BaseResponse.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace PetEcommerce.Domain.BaseReponse
+{
+    public class BaseResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+
+        public BaseResponse() { }
+
+        public BaseResponse(T data, bool isSuccess, HttpStatusCode statusCode, string message)
+        {
+            Data = data;
+            IsSuccess = isSuccess;
+            StatusCode = statusCode;
+            Message = message;
+        }
+
+        public BaseResponse(bool isSuccess, HttpStatusCode statusCode, string message)
+        {
+            IsSuccess = isSuccess;
+            StatusCode = statusCode;
+            Message = message;
+        }
+    }
+}
---
+++ PetEcommerce.Domain/BaseReponse/ResponseHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace PetEcommerce.Domain.BaseReponse
+{
+    public class ResponseHandler
+    {
+        public BaseResponse<T> Success<T>(T data)
+        {
+            return new BaseResponse<T>(data, true, HttpStatusCode.OK, "Operation completed successfully.");
+        }
+
+        public BaseResponse<T> Failure<T>(T data, HttpStatusCode statusCode, string errorMessage)
+        {
+            return new BaseResponse<T>(data, false, statusCode, errorMessage);
+        }
+
+        public BaseResponse<T> Created<T>(T data)
+        {
+            return new BaseResponse<T>(data, true, HttpStatusCode.Created, "Resource created successfully.");
+        }
+
+        public BaseResponse<T> Updated<T>(T data)
+        {
+            return new BaseResponse<T>(data, true, HttpStatusCode.Accepted, "Resource updated successfully.");
+        }
+
+        public BaseResponse<T> NotFound<T>()
+        {
+            return new BaseResponse<T>(false, HttpStatusCode.BadRequest, "Resource not Found.");
+        }
+    }
+}
