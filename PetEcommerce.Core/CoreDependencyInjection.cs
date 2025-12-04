355299126c9da8d5d333419c7f4322604a4f1239
Yusuf Mostafa <yusufmustafa1@outlook.com>
2025-12-04 14:15:10 +0200

    [Add] Core dependency injection and register the required services

---
+++ PetEcommerce.Core/CoreDependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using PetEcommerce.Core.Behavior;
+using PetEcommerce.Core.Mapping;
+using System.Reflection;
+
+namespace PetEcommerce.Core
+{
+    public static class CoreDependencyInjection
+    {
+        public static IServiceCollection AddCoreDependencyInjection(this IServiceCollection services)
+        {
+            // Add MediatR for CQRS
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(CoreDependencyInjection).Assembly);
+                cfg.AddOpenBehavior(typeof(ValidatorBehavior<,>));
+            });
+            // Add Fluentvalidation 
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            // Add AutoMapper for object mapping
+            services.AddAutoMapper(cfg => cfg.AddProfile(typeof(Profiling)));
+            return services;
+        }
+    }
+}
