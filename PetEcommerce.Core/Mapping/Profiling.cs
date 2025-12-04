509b026e32b17d844c2bf10b80d07a86112865b1
Yusuf Mostafa <yusufmustafa1@outlook.com>
2025-12-04 14:15:40 +0200

    [Add] Main Profile from automapper

---
+++ PetEcommerce.Core/Mapping/Profiling.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using PetEcommerce.Domain.Pagination;
+
+namespace PetEcommerce.Core.Mapping
+{
+    public partial class Profiling : Profile
+    {
+        public Profiling()
+        {
+            CreateMap(typeof(PagedResult<>), typeof(PagedResult<>));
+            PetConfigurationMapping();
+            ToyConfigurationMapping();
+        }
+
+    }
+}
