2545f2cceae77feb7788a5933cbab6e5baa77096
Yusuf Mostafa <yusufmustafa1@outlook.com>
2025-12-04 14:09:13 +0200

    [Add] Service Dependency injection

---
+++ PetEcommerce.Service/ServiceDependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetEcommerce.Service.BaseService;
+
+namespace PetEcommerce.Service
+{
+    public static class ServiceDependencyInjection
+    {
+        public static IServiceCollection AddServiceDependencyInjection(this IServiceCollection services)
+        {
+            // Register service layer dependencies here
+            // e.g., services.AddScoped<IYourService, YourServiceImplementation>();
+            services.Scan(scan => scan
+                .FromAssemblyOf<PetService>()
+                .AddClasses()
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+            return services;
+        }
+    }
+}
