b3eb63e5650756095eba1ba5bd05588e78f6e331
Yusuf Mostafa <yusufmustafa1@outlook.com>
2025-12-04 14:13:31 +0200

    [Add] Global exception handler middleware

---
+++ PetEcommerce.Core/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PetEcommerce.Core.Middleware
+{
+    public class GlobalExceptionMiddleware
+    {
+        #region Fields
+        private readonly RequestDelegate _next;
+        #endregion
+
+        #region Constructors
+        public GlobalExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        #endregion
+
+        #region Methods
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+                var response = ex.Message;
+                await context.Response.WriteAsync(response);
+            }
+        }
+        #endregion
+    }
+}
