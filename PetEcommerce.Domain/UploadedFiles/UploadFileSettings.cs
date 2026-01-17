49c7fc2eaecbc5ef48eaec6d9a4b3cb0cce54813
yusuf mustafa kamel <yusufmustafa1@outlook.com>
2026-01-17 14:23:19 +0200

    [Add] UploadFilesSettings to Options Patterns

---
+++ PetEcommerce.Domain/UploadedFiles/UploadFileSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace PetEcommerce.Domain.UploadedFiles
+{
+    public class UploadFileSettings
+    {
+        public const string SectionName = "UploadFileSettings";
+        public string UploadFolderPath { get; set; } = string.Empty;
+        public long MaxFileSizeInBytes { get; set; }
+        public List<string> AllowedFileExtensions { get; set; } = new List<string>();
+    }
+}
